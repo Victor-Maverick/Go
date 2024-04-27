268b601cf28900e79cf1c78604910ee2f2186e11
Victor-Maverick <victormsonter@gmail.com>
2024-04-27 14:55:30 +0100

    commit

--- chapterFour/salesCommissionCalculator/SalesCommissionCalculator.go
+++ chapterFour/salesCommissionCalculator/SalesCommissionCalculator.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-
+	processCommission()
 }
 
 func collectNumberOfItems() int64 {
 	numberOfItemCategories := 0
 	fmt.Println("Enter the number of item categories you sold: ")
-	fmt.Scan(numberOfItemCategories)
+	fmt.Scan(&numberOfItemCategories)
 	return int64(numberOfItemCategories)
 }
 
 func collectValueOfItem() float64 {
 	var valueOfItem float64
 	fmt.Println("Enter the value of item you sold: ")
-	fmt.Scan(valueOfItem)
+	fmt.Scan(&valueOfItem)
 	return valueOfItem
 }
 
@@ -24,10 +24,10 @@ func processCommission() {
 	totalSales := 0.0
 	numberOfItems := collectNumberOfItems()
 	for i := 0; i < int(numberOfItems); i++ {
-		fmt.Println("Enter the price of item ", i+1)
+		fmt.Println("Item ", i+1)
 		priceOfItem := collectValueOfItem()
 		totalSales += priceOfItem
 	}
-	earnings := 0.9*totalSales + 200
+	earnings := 0.09*totalSales + 200
 	fmt.Println("Your accumulated earning for the week is ", earnings)
 }
