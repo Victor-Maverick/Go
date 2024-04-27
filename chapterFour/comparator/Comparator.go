ad481a090a11285032e4942e3257c06abd0e0ef6
Victor-Maverick <victormsonter@gmail.com>
2024-04-28 00:09:54 +0100

    commit

--- chapterFour/comparator/Comparator.go
+++ chapterFour/comparator/Comparator.go
@@ -1,1 +1,28 @@
-package comparator
+package main
+
+import "fmt"
+
+func main() {
+	firstNumber := 0
+	secondNumber := 0
+
+	fmt.Println("Enter First Number: ")
+	_, err := fmt.Scan(&firstNumber)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Enter Second Number: ")
+	_, err = fmt.Scan(&secondNumber)
+	if err != nil {
+		return
+	}
+
+	if firstNumber == secondNumber {
+		fmt.Println(0)
+	} else if firstNumber > secondNumber {
+		fmt.Println(1)
+	} else if firstNumber < secondNumber {
+		fmt.Println(-1)
+	}
+}
