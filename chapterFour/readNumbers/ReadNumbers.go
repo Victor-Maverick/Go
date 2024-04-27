fb4d1739ec6902e921b04d870352518e2468d94c
Victor-Maverick <victormsonter@gmail.com>
2024-04-28 00:03:11 +0100

    commit

--- chapterFour/readNumbers/ReadNumbers.go
+++ chapterFour/readNumbers/ReadNumbers.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	number := 0
+	numbers := 0
+	total := 0
+	fmt.Println("Enter a number")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		return
+	}
+	for total >= number {
+		fmt.Println("Enter another number: ")
+		_, err := fmt.Scan(&numbers)
+		if err != nil {
+			return
+		}
+		total += numbers
+	}
 }
