a75d17f2aefd14a1e81c9220ca426ba8f30030cb
Victor-Maverick <victormsonter@gmail.com>
2024-04-28 17:22:35 +0100

    commit

---
+++ chapterFive/trianglePrints/TrianglePrints.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	printShapes()
+}
+
+func printShapes() {
+	for count := 0; count < 10; count++ {
+		for index := 0; index <= count; index++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+	for count := 10; count > 0; count-- {
+		for index := 0; index < count; index++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+}
