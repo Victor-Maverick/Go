71905c2227264b9918fe06a88679b7c3a1eb0267
Victor-Maverick <victormsonter@gmail.com>
2024-04-28 16:02:40 +0100

    commit

--- chapterFour/population/Population.go
+++ chapterFour/population/Population.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	displayWorldPopulation()
 }
@@ -7,10 +9,13 @@ func main() {
 func displayWorldPopulation() {
 	worldPopulation := 7975105156.00
 	increaseRate := 0.89
-
+	fmt.Println("-------------------------------------------------------------")
+	fmt.Println("Year\t\tExpected Population\t\t\tNumeric Increase")
+	fmt.Println("-------------------------------------------------------------")
 	for count := 1; count <= 75; count++ {
 		increase := increaseRate * worldPopulation
 		worldPopulation += increase
-
+		fmt.Println(count, "\t\t", worldPopulation, "\t\t", increase)
 	}
+	fmt.Println("-------------------------------------------------------------")
 }
