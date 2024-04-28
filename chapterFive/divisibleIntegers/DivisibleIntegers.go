d1478eaae400f65e595ab48a06f8abde598037bd
Victor-Maverick <victormsonter@gmail.com>
2024-04-28 16:34:14 +0100

    commit

--- chapterFive/divisibleIntegers/DivisibleIntegers.go
+++ chapterFive/divisibleIntegers/DivisibleIntegers.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+func main() {
+	sumOfThreeDivisibles := 0
+	for count := 1; count < 30; count++ {
+		if count%3 == 0 {
+			sumOfThreeDivisibles += count
+		}
+	}
+	fmt.Println("The sum of integers between 1 and 30 that are divisible by 3 are", sumOfThreeDivisibles)
+}
