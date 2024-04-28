8439d17def6a3f83c28e27573c19d2e61478ce44
Victor-Maverick <victormsonter@gmail.com>
2024-04-28 15:33:59 +0100

    commit

--- chapterFour/cryptography2/decrypt.go
+++ chapterFour/cryptography2/decrypt.go
@@ -1,13 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	decryptData()
 }
 
 func decryptData() {
-	collectEncryptedData()
+	userInput := collectEncryptedData()
+	swappedDigits := swapDigits(userInput)
+	returnDigit := ""
+
+	for count := 0; count < len(swappedDigits); count++ {
+		number, _ := strconv.ParseInt(string(swappedDigits[count]), 10, 64)
+		number = (number + 3) % 10
+		returnDigit += strconv.FormatInt(number, 10)
+	}
+	fmt.Println(returnDigit)
+}
+
+func swapDigits(input string) string {
+	var swappedString string
+	swappedString += string(input[2])
+	swappedString += string(input[3])
+	swappedString += string(input[0])
+	swappedString += string(input[1])
+
+	return swappedString
+
 }
 
 func collectEncryptedData() string {
